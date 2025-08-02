70c30ec46a8584f6f9d841f2d56a9219c1705752
sireethorn <sireethorn2548.h20p@gmail.com>
2025-08-02 15:20:51 +0700

    sub

--- src/App.tsx
+++ src/App.tsx
@@ -3,57 +3,50 @@ import { Footer } from "./components/Footer";
 import { Sidebar } from "./components/Sidebar";
 import { TaskInput } from "./components/TaskInput";
 import { Task } from "./components/Task";
-import { type TaskCardProps } from "./libs/types";
 
 function App() {
-  const TaskCards: TaskCardProps[] = [
-    {
-      id: 1,
-      title: "Read a book",
-      description: "Vite + React + Bootstrap + TS",
-      isDone: false,
-    },
-    {
-      id: 2,
-      title: "Write code",
-      description: "Finish project for class",
-      isDone: false,
-    },
-    {
-      id: 3,
-      title: "Deploy app",
-      description: "Push project to GitHub Pages",
-      isDone: false,
-    },
-  ];
-
   return (
     <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-       {/* Header ด้านบน */}
+      {/* Header ด้านบน */}
       <Header></Header>
+
       <div className="d-flex flex-grow-1">
+        {/* Sidebar ด้านซ้าย */}
+        <Sidebar userName="Sireethorn" type="student"></Sidebar>
         {/* Main content */}
         <div className="container text-center">
           <main className="flex-grow-1 p-4">
-        {/* input ด้านบน */}
-            <TaskInput />
-
+            {/* input ด้านบน*/}
+            <TaskInput></TaskInput>
             {/* Card รายการ */}
-            <div className="col-12 m-2 p-0">
-              <Task {...TaskCards[0]} />
-              <Task {...TaskCards[1]} />
-              <Task {...TaskCards[2]} />
-            </div>
+            <Task
+              id={1}
+              title="Read a book"
+              description="Vite + React + Bootstrap + TS"
+              isDone={false}
+            ></Task>
+            <Task
+              id={2}
+              title="Write a code"
+              description="Finish project for class"
+              isDone={false}
+            ></Task>
+            <Task
+              id={3}
+              title="Deploy app"
+              description="Push to GitHub pages"
+              isDone={false}
+            ></Task>
           </main>
         </div>
       </div>
 
       {/* Footer ด้านล่าง */}
       <Footer
-        year="2026"
+        year="2025"
         fullName="Sireethorn Pinthong"
         studentId="670610741"
-      />
+      ></Footer>
     </div>
   );
 }
